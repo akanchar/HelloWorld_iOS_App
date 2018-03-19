839880218b5313a003af630f9e61d2d6526a36dd
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-03-19 17:10:24 -0500

    Test

--- HelloWorld/AppDelegate.swift
+++ HelloWorld/AppDelegate.swift
@@ -12,13 +12,22 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    struct Digit {
+        var number : Int
+        init(_ number: Int) {
+            self.number = number
+        }
+        
+    }
+    private var x :Int = 1
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        var julie = Dog("julie")
+        julie.bark()
+        julie.walk()
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -40,7 +49,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+}
+class FourLeggedCreature {
+    init(_ name:String) {
+        print("My name is",name)
+    }
+    func walk(){
+        print("walk walk walk")
+    }
+}
+class Dog : FourLeggedCreature {
+    override init(_ name: String) {
+        super.init(name)
+    }
+    func bark() {
+        print("woof")
+    }
+    override func walk() {
+        print("walk")
+    }
 }
 
